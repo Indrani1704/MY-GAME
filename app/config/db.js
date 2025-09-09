5042bd22821f2892957d0c679e5f22a3abde1960
Indrani Mukhopadhyay <indranimukhopadhyay17@gmail.com>
2025-09-10 02:17:07 +0530

    Added lobby and game views

--- app/config/db.js
+++ app/config/db.js
@@ -2,15 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
   } catch (err) {
-    console.error(`❌ Error: ${err.message}`);
-    process.exit(1); // Exit on failure
+    console.error("❌ MongoDB connection failed", err);
+    process.exit(1);
   }
 };
 
